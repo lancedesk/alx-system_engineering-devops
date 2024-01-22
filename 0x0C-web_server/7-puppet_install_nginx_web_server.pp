25669ead072a7a1756ffa0ba8c1da84403769a60
Lance Desk <lncedesk@gmail.com>
2024-01-22 07:40:43 -0800

    Install Nginx web server (w/ Puppet)

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,42 @@
+$default_file = 'https://raw.githubusercontent.com/lancedesk/alx-system_engineering-devops/master/0x0C-web_server/default'
+$default_file_location = '/etc/nginx/sites-available/default'
+
+# Run apt-get update
+exec { 'apt-update':
+  command => '/usr/bin/apt-get update',
+}
+
+# Install Nginx package
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['apt-update'],
+}
+
+# Create a default index.html
+file { 'Create index.html':
+  require => Package['nginx'],
+  path    => '/var/www/html/index.html',
+  content => 'Hello World!\n',
+}
+
+# Create a default 404 error page
+file { 'Create 404.html':
+  require => Package['nginx'],
+  path    => '/var/www/html/404.html',
+  content => 'Ceci n\'est pas une page\n',
+}
+
+# Configure Nginx main configuration file and restart Nginx
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+  content => file($default_file),
+}
+
+# Ensure Nginx service is running and enabled
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => File['/etc/nginx/sites-available/default'],
+}
